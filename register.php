049ddeb4786c87469a712280ef4bac874fea3e32
Tadashi Konno <jensonnumber1@gmail.com>
2022-12-30 14:14:05 -0800

    regsitration view is created

---
+++ register.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="./" method="POST">
+    <div>
+      <label for="username">User Name</label>
+      <input type="text" id="username" name="username" />
+    </div>
+    <div>
+      <label for="password">Password</label>
+      <input type="password" id="password" name="password" />
+    </div>
+    <input type="button" name="submit" value="Submit" />
+  </form>
+</body>
+</html>
