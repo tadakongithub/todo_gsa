3ca58826a01a56eafc5346a844abcac81b7944fb
Tadashi Konno <jensonnumber1@gmail.com>
2023-01-02 12:52:58 -0800

    db credential replace with env values

--- db/db.php
+++ db/db.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('DB_NAME', 'todo_gsa');
+define('DB_HOST', $_ENV['DB_HOST']);
+define('DB_USER', $_ENV['DB_USER']);
+define('DB_PASS', $_ENV['DB_PASS']);
+define('DB_NAME', $_ENV['DB_NAME']);
 
 try {
   $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
